3749b68ff3490e53e1354f3a2021b95c2c750527
filipecamargos <filipe.atrabalho@gmail.com>
2021-05-12 20:47:23 -0600

    Provider set

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,19 @@
-import {createStore} from 'redux';
+import { createStore } from "redux";
 
 //Reducer function to be executed
-const counterReducer = (state = {counter = 0}, action) => {
-    switch (action.type) {
-        case 'increment':
-            return {
-                counter: state.counter + 1
-            }
-        case 'decrement':
-            return {
-                counter: state.counter - 1
-            }
-        default:
-            return state;
-    }
+const counterReducer = (state = { counter: 0 }, action) => {
+  switch (action.type) {
+    case "increment":
+      return {
+        counter: state.counter + 1,
+      };
+    case "decrement":
+      return {
+        counter: state.counter - 1,
+      };
+    default:
+      return state;
+  }
 };
 
 //Create the store
