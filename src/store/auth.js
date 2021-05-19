9a05fb30e7464ec03e07383ce4fc453e200e963a
filipecamargos <filipe.atrabalho@gmail.com>
2021-05-18 18:32:16 -0600

    Auth slice in new file

---
+++ src/store/auth.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+//initial state to manage auth
+const authInitialstate = { isAuthenticated: false };
+
+//slice for the auth
+const authSlice = createSlice({
+  name: "auth",
+  initialState: authInitialstate,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
+
+export default authSlice;
